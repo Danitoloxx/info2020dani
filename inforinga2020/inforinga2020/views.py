42496d026f528308f06ee18b0c4d5e8cd557d761
Danitoloxx <danitoloxx@gmail.com>
2020-09-04 23:43:34 -0300

    agregado el login y el logout

--- inforinga2020/inforinga2020/views.py
+++ inforinga2020/inforinga2020/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def Home(request):
     return render(request,'index.html')
 
+def Login(request):
+    return render(request,'usuarios/login.html')
+
 def Registro(request):
     return render(request,'registro.html')
